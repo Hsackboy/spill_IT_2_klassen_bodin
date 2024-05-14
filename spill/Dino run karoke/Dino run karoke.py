a2fe0a7c21403877777417743a0d4a2260e05603
=Håkon Mikalsen <=hakmikalsen@gmail.com>
2024-05-14 15:21:17 +0200

    gjorde ferdig dino karoke, den er ganske kul

--- spill/Dino run karoke/Dino run karoke.py
+++ spill/Dino run karoke/Dino run karoke.py
@@ -17,9 +17,12 @@ timeingPath = r"/data/lineTimeing.json"
 lyrics = lesHelFil(folderPath+lyricPath)
 lyrics= lyrics.split("\n")
 
-backgroundColor = (230,230,230)
+backgroundColor = (255,255,255)
+hasSungColor = (0,0,0)
+ComingUp = (200,200,200)
 
 timeing = lesJson(folderPath+timeingPath)
+adjustTime = 700
 # print(timeing)
 
 pg.init()
@@ -32,10 +35,13 @@ backgroundTrackChannel.play(song)
 # print(backgroundTrackChannel.get_busy())
 
 
-vindu_bredde, vindu_høyde = 500,500
+vindu_bredde, vindu_høyde = 500,700
+
+topBotMargin = 20
+spacePerLine = (vindu_høyde-topBotMargin*2)/len(lyrics)
 
 # lager 2 fonts, den andre fonten brukes til å ha understrek under slik at ikke all teksten får understrek under seg
-font = pg.font.SysFont('arial', 36)
+font = pg.font.SysFont('arial', 20)
 
 vindu = pg.display.set_mode((vindu_bredde, vindu_høyde))
 #starter spill
@@ -53,6 +59,20 @@ while fortsett:
     
     clock.tick(FPS)
     ingameTime+=clock.get_time()
+    
+    pos = 0
+    linecount = 1
+    for line in lyrics:
+        if timeing[str(linecount)]-adjustTime<ingameTime:
+            tekst = font.render(line, True, hasSungColor)
+        else:
+            tekst = font.render(line, True, ComingUp)
+            
+        textSize = tekst.get_size()
+        vindu.blit(tekst, (vindu_bredde/2 -textSize[0]/2,topBotMargin+spacePerLine*pos))
+        pos+=1
+        linecount+=1
+        
     
     trykkede_taster = pg.key.get_pressed()
     if trykkede_taster[pg.K_SPACE]:
